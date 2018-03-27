0b39453442e01b8604ce8e42bcfe1373ccd9cbfc
parin <pate0468@algonquinlive.com>
2018-03-26 23:15:29 -0400

    First Commit

--- app/src/main/java/com/example/parin/finalproject/MainActivity.java
+++ app/src/main/java/com/example/parin/finalproject/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+//This is main activity
 
 public class MainActivity extends Activity {
 
